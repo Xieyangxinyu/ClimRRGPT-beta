ad3baae3fc89ba84ff495941427e2b4a4d9c12c2
Yangxinyu <xinyux@wharton.upenn.edu>
2024-05-16 04:17:10 +0800

    auto

--- src/evaluation/auto.py
+++ src/evaluation/auto.py
@@ -1,5 +1,7 @@
 from sentence_transformers import SentenceTransformer
 import torch
+from rouge import Rouge
+
 
 sbert_model = SentenceTransformer('all-MiniLM-L6-v2')
 cossim = torch.nn.CosineSimilarity(dim=0, eps=1e-6)
@@ -7,3 +9,9 @@ cossim = torch.nn.CosineSimilarity(dim=0, eps=1e-6)
 def score_sbert_similarity(text1, text2):
     embeddings = sbert_model.encode([text1, text2], convert_to_tensor=True)
     return cossim(embeddings[0], embeddings[1]).item()
+
+# get the similarity score between two texts using ROUGE score
+def score_rouge_similarity(text1, text2):
+    rouge = Rouge()
+    scores = rouge.get_scores(text1, text2)
+    return scores[0]['rouge-l']['f']
